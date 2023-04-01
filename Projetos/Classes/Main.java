1f24d05f6d95f2de0a0717edaae70ae10cad33d4
Dantas_Dev02 <98811096+Gabr002@users.noreply.github.com>
2023-04-01 16:29:49 -0300

    Aprendendo programação orientado objeto.

---
+++ Projetos/Classes/Main.java
@@ -0,0 +1,20 @@
+package Classes;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Paradigmas de orintação objeto
+        // Programação Estruturada e Programação Orientada a Objetos
+        // Trabalhando com métodos
+        // Entendendo o que é uma classe.
+        Pessoa objetoPessoa = new Pessoa(); // 
+        
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o peso da pessoa: ");
+        
+        System.out.println("Digite a altura da pessoa: ");
+        // 06:09 Aula 4.T
+    }
+}
---
+++ Projetos/Classes/Pessoa.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Pessoa {
+
+    // Atributos
+    float peso;
+    float altura;
+
+    public float calcularIMC() {
+        float imc = peso/ (altura*altura);
+        return imc;
+    }
+
+}
