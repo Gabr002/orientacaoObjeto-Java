d7596656c3aadc9ca33747ced6edef252eca5095
Dantas_Dev02 <98811096+Gabr002@users.noreply.github.com>
2023-04-03 22:06:23 -0300

    Aprendendo um novo conteúdo.
    - Herança.

---
+++ Projetos/Capgimini/src/Heranca/Main.java
@@ -0,0 +1,13 @@
+package Heranca;
+import Heranca.Classes.Motorista;
+import Heranca.Classes.Vendedor;
+
+public class Main {
+    public static void main(String[] args) {
+        Vendedor v = new Vendedor();
+        // v.setNome("");
+        Motorista m = new Motorista();
+
+        // É uma aplicação onde irá reduzir os códigos que está duplicado.
+    }
+}
